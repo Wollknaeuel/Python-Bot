c43226ca47508e646b5af7a33eff58a1000ed883
Wollknäuel <89309980+Wollknaeuel@users.noreply.github.com>
2021-10-05 19:14:43 +0200

    Add files via upload

--- DiscordBot1.py
+++ DiscordBot1.py
@@ -1,33 +1,69 @@
 import discord
 
-class MyClient(discord.Client):
-    #Einloggen
-    async def on_ready(self):
-            print("Ja mein Herr und Gebieter, ich bin zurstelle.")
 
-    #Reaktion auf Nachrichten
-    async def on_message(self, message):
-        if message.author == client.user:
-            return
+import random
 
-        if message.content.startswith("hallo"):
-            await message.channel.send("Willkommen mein Gebieter")
+import datetime
 
-        if message.content.startswith("moin"):
-            await message.channel.send("Willkommen mein Gebieter")
+from discord import Member
+from discord.utils import get
 
-        if message.content.startswith("Hallo"):
-            await message.channel.send("Willkommen mein Gebieter")
+import asyncio
 
-        if message.content.startswith("Moin"):
-            await message.channel.send("Willkommen mein Gebieter")
+import time
 
-        if message.content.startswith("Malzeit"):
-            await message.channel.send("Willkommen mein Gebieter")
+client = discord.Client()
 
-        if message.content.startswith("malzeit"):
-            await message.channel.send("Willkommen mein Gebieter")
+@client.event
+async def on_ready():
+    print('Ich bin da mein Gebieter, ich bin {}'.format(client.user.name))
+    client.loop.create_task(status_task())
 
 
-client = MyClient()
-client.run("hier das Token einfügen")
+#Status Anzeige welches spiel gespielt wird.
+async def status_task():
+    while True:
+     await client.change_presence(activity=discord.Game('FF14'), status=discord.Status.online)
+     await asyncio.sleep(5)
+     await client.change_presence(activity=discord.Game('My Little Pony :D'), status=discord.Status.online)
+     await asyncio.sleep(5)
+
+
+@client.event
+async def on_message(message):
+    if message.author.bot:
+        return
+    if 'Sklave' in message.content:
+        await message.channel.send('Ja Herr, zu diensten')
+    if message.content.startswith('!userinfo'):
+        args = message.content.split(' ')
+        if len(args) == 2:
+            member: Member = discord.utils.find(lambda m: args[1] in m.name, message.guild.members)
+            if member:
+                embed = discord.embed(title='Userinfo für {}'.format(member.name),
+                                      description='Dies ist eine Userinfo für den User {}'.format(member.mention),
+                                      color=0x22a7f0)
+                embed.add_field(name='Server beigetreten', value=member.joined_at.strftime('%d/%m/%Y, %H:%M'),
+                                inline=True)
+                embed.add_field(name='Discord beigetreten', value=member.created_at.strftime('%d/%m/%Y, %H:%M'),
+                                inline=True)
+                rollen = ''
+                for role in member.roles:
+                    if not role.is_default():
+                        rollen += '{} \r\n'.format(role.mention)
+                if rollen:
+                    embed.add_field(name='Rollen', value=rollen, inline=True)
+                embed.set_thumbnail(url=member.avatar_url)
+                embed.set_footer(text='Ich bin ein EmbedFooter.')
+                await message.channel.send(embed=embed)
+
+        if message.content.startswith('Moin'):
+            await message.channel.send('Willkommen werter Gast.')
+        if message.content.startswith('Hallo'):
+            await message.channel.send('Willkommen werter Gast.')
+        if message.content.startswith('Malzeit'):
+            await message.channel.send('Willkommen werter Gast.')
+
+
+
+client.run("hier das Token")
